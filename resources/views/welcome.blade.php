bd4980523894698aa0524068b871be65af5c13f6
kei <dr.kei21@gmail.com>
2023-01-17 06:47:28 +0900

    faviconの変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="icon" href="{{ url('../../public/favicon.ico') }}">
+    <link rel="icon" href="{{ url('favicon.ico') }}">
     <link rel="stylesheet" href="../css/app.css">
-    <title>WK</title>
+    <title>portfolio</title>
 </head>
 
 <body>
