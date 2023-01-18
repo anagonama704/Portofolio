6a07d30307ac599eebae95cae36801f8a0789fbd
kei <dr.kei21@gmail.com>
2023-01-18 16:24:00 +0900

    add：入力エリアの追加

--- resources/ts/components/Contact.tsx
+++ resources/ts/components/Contact.tsx
@@ -6,14 +6,35 @@ const Contact = () => {
     return (
         <div id="contact">
             <Card id="co_card">
-                <h2>contact</h2>
-                <TextField
-                    id="filled-multiline-flexible"
-                    label="Multiline"
-                    multiline
-                    maxRows={5}
-                    variant="filled"
-                />
+                <form action="" method="">
+                    <div className="forms">
+                        <h2>contact</h2>
+                        <TextField
+                            className="u_name"
+                            label="お名前"
+                            name="username"
+                            multiline
+                            maxRows={5}
+                            variant="filled"
+                        />
+                        <TextField
+                            className="u_name"
+                            label="お名前"
+                            name="username"
+                            multiline
+                            maxRows={5}
+                            variant="filled"
+                        />
+                        <TextField
+                            className="u_name"
+                            label="お名前"
+                            name="username"
+                            multiline
+                            maxRows={5}
+                            variant="filled"
+                        />
+                    </div>
+                </form>
             </Card>
         </div>
     );
