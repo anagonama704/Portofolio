822fc3ada4742aba31eff914b37ab2ec976492bb
kei <dr.kei21@gmail.com>
2023-01-24 11:13:40 +0900

    update：コンタクトページのデザイン修正

--- resources/ts/components/Contact.tsx
+++ resources/ts/components/Contact.tsx
@@ -1,38 +1,62 @@
 import React from "react";
-import { TextField, Card } from "@mui/material";
+import { TextField, Card, Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 import "../../css/contact.css";
 
 const Contact = () => {
     return (
         <div id="contact">
             <Card id="co_card">
-                <form action="" method="">
+                <form action="http://localhost:8000/api/mails" method="post">
                     <div className="forms">
                         <h2>contact</h2>
-                        <TextField
-                            className="u_name"
-                            label="お名前"
-                            name="username"
-                            multiline
-                            maxRows={5}
-                            variant="filled"
-                        />
-                        <TextField
-                            className="u_name"
-                            label="お名前"
-                            name="username"
-                            multiline
-                            maxRows={5}
-                            variant="filled"
-                        />
-                        <TextField
-                            className="u_name"
-                            label="お名前"
-                            name="username"
-                            multiline
-                            maxRows={5}
-                            variant="filled"
-                        />
+                        <div className="center">
+                            <TextField
+                                className="u_name"
+                                label="お名前"
+                                name="username"
+                                multiline
+                                maxRows={5}
+                                variant="filled"
+                                style={{
+                                    margin: "50px 0 0 0",
+                                }}
+                            />
+                            <TextField
+                                className="mail"
+                                label="メールアドレス"
+                                name="mails"
+                                multiline
+                                maxRows={5}
+                                variant="filled"
+                                style={{
+                                    margin: "30px 0 0 0",
+                                }}
+                            />
+                            <TextField
+                                className="content"
+                                label="お問い合わせ内容"
+                                name="content"
+                                multiline
+                                maxRows={5}
+                                variant="outlined"
+                                style={{
+                                    backgroundColor: "#00000010",
+                                    margin: "50px 0 0 0",
+                                }}
+                                size="medium"
+                            />
+                        </div>
+                        <div id="btn">
+                            <Button
+                                variant="contained"
+                                endIcon={<SendIcon />}
+                                type="submit"
+                                className="sbm_btn"
+                            >
+                                送信する
+                            </Button>
+                        </div>
                     </div>
                 </form>
             </Card>
